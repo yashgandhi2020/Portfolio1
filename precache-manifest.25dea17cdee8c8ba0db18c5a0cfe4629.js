41c7c35d25efd36d661bdd3d57de9778e6bda99d
Yash Gandhi <yashgandhi2020@gmail.com>
2020-04-21 01:29:31 +0530

    Updates

---
+++ precache-manifest.25dea17cdee8c8ba0db18c5a0cfe4629.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c34269088e56fc78da689f2aca0ff01",
+    "url": "/yashgandhi2020.github.io/index.html"
+  },
+  {
+    "revision": "bee2b0c21a7dc3a3aef7",
+    "url": "/yashgandhi2020.github.io/static/css/main.d7f5c056.chunk.css"
+  },
+  {
+    "revision": "dac92547a88d3566ef80",
+    "url": "/yashgandhi2020.github.io/static/js/2.309a854a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yashgandhi2020.github.io/static/js/2.309a854a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee2b0c21a7dc3a3aef7",
+    "url": "/yashgandhi2020.github.io/static/js/main.63dabdd1.chunk.js"
+  },
+  {
+    "revision": "35f2e0625023c34de16c",
+    "url": "/yashgandhi2020.github.io/static/js/runtime-main.2885e07f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yashgandhi2020.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ff6e9861b18def17a66d06c43239317a",
+    "url": "/yashgandhi2020.github.io/static/media/netlify.ff6e9861.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/yashgandhi2020.github.io/precache-manifest.25dea17cdee8c8ba0db18c5a0cfe4629.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yashgandhi2020.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
